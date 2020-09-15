44f4c0310c6ec4bba2db768814209197c8ac0d1e
Harshil Srivastava <harshil.srivastava758@gmail.com>
2020-09-15 12:09:27 +0530

    Implmented jwt.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 // @route   POST api/users
@@ -46,9 +48,20 @@ router.post('/', [
             user.password = await bcrypt.hash(password, salt);
             await user.save();
 
-            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User Registered');
+            jwt.sign(
+                payload,
+                config.get('jwtToken'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                });
         } catch (err) {
             console.log(err.message);
             res.status(500).send('Server Error');
